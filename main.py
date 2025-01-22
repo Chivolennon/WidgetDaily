209b24068aa6d9e41cb3ac5d05547dd900e18348
Simón Sánchez <chivolennon@gmail.com>
2025-01-22 16:31:04 -0300

    Start Solution

---
+++ main.py
@@ -0,0 +1,5 @@
+# "#" SE USA PARA COMENTAR COSAS. USAREMOS TAMBIÉN "tkinter" para crear luego la interfaz GUI.
+# Windows ya lo tiene instalado.
+
+print("Primera prueba para solución Widget Daily.")
+
